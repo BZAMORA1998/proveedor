a50fc9e2aebd9af445946e03932342f64cd8f1d7
Bryan <bryan.zamora@goitsa.me>
2021-05-24 22:52:57 -0500

    Se agrega modal

---
+++ sistema-proveedor-api/sistema-proveedor-api/MODEL/TiposIdentificacion.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_proveedor_api.MODEL
+{
+    public class TiposIdentificacion
+    { 
+        public int SECUENCIA_TIPO_IDENTIFICACION { get; set; }
+        public string es_activo { get; set; }
+        public string nombre { get; set; }
+    }
+}
